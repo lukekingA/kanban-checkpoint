3e99170e2c34d71c9f5a0db02dbadd9e8f854008
Luke King <lkingfrm@gmail.com>
2019-03-11 17:06:46 -0600

    routes complete

--- server/server-assets/routes/task.js
+++ server/server-assets/routes/task.js
@@ -3,7 +3,9 @@ let Tasks = require('../models/task')
 
 //GET  -- ? boardsId also ? readability
 router.get('/lists/:id', (req, res, next) => {
-  Tasks.find({ listId: req.params.id })
+  Tasks.find({
+      listId: req.params.id
+    })
     .then(data => {
       res.send(data)
     })
@@ -13,12 +15,11 @@ router.get('/lists/:id', (req, res, next) => {
     })
 })
 
-//POST  
-router.post('/boards/:boardId/lists/:listId', (req, res, next) => {
+//POST  Task
+router.post('/lists/:listId', (req, res, next) => {
   req.body.authorId = req.session.uid
-  req.body.boardId = req.params.boardId
   req.body.listId = req.params.listId
-  Lists.create(req.body)
+  Tasks.create(req.body)
     .then(newList => {
       res.send(newList)
     })
@@ -28,17 +29,56 @@ router.post('/boards/:boardId/lists/:listId', (req, res, next) => {
     })
 })
 
-//PUT
-router.put('/tasks/:id', (req, res, next) => {
-  Tasks.findById(req.params.id)
-
+//Post Comment
+router.post('/tasks/:id', (req, res, next) => {
+  req.body.authorId = req.session.uid
+  Tasks.findById(req.params.id).then(task => {
+      task.comments.push(req.body)
+    })
+    .catch(err => {
+      console.log(err)
+      next()
+    })
 })
 
 
+//PUT Task
+router.put('/tasks/:id', (req, res, next) => {
+  Tasks.findById(req.params.id).then(task => {
+    if (!task.authorId.equals(req.session.uid)) {
+      return res.status(401).send("ACCESS DENIED!")
+    }
+    task.update(req.body, (err) => {
+      if (err) {
+        console.log(err)
+        next()
+      }
+    })
+  })
 
-//DELETE
+})
+
+//Delete Comment
+
+router.delete('/tasks/:taskId/comment/:commentId', (req, res, next) => {
+  Tasks.findById(req.params.taskId).then(task => {
+    let comment = task.comments.find(c => c._id == req.params.commentId)
+    if (comment) {
+      task.comments.splice(task.comments.indexOf(comment), 1)
+    }
+  }).then(comment => {
+    res.send('Comment Deleted')
+  }).catch(err => {
+    res.status(400).send('ACCESS DENIED; Invalid Request')
+  })
+})
+
+//DELETE Task
 router.delete('/tasks/:id', (req, res, next) => {
-  Tasks.findOneAndDelete({ _id: req.params.id, authorId: req.session.uid })
+  Tasks.findOneAndDelete({
+      _id: req.params.id,
+      authorId: req.session.uid
+    })
     .then(list => {
       res.send("Successfully Deleted")
     })
