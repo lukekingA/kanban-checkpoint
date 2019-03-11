d6f5ae982a7aa4a4b7b9fb4503dbfaf955d61c38
Luke King <lkingfrm@gmail.com>
2019-03-11 13:44:42 -0600

    updated database

--- server/server-assets/db/gearhost-config.js
+++ server/server-assets/db/gearhost-config.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose')
-var connectionString = "mongodb://markkanban:He02R~9I82-M@den1.mongo1.gear.host:27001/markkanban" //CHANGE ME!!!!!
+var connectionString = "mongodb://kanbanml:coffee!@den1.mongo1.gear.host:27001/kanbanml" //CHANGE ME!!!!!
 var connection = mongoose.connection
 
 
-mongoose.connect(connectionString, { useMongoClient: true })
+mongoose.connect(connectionString, {
+  useMongoClient: true
+})
 
 connection.on('error', err => {
   console.log('ERROR FROM DATABASE: ', err)
