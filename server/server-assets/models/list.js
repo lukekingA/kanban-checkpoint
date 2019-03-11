c97aad68cb44c88724a5f911a40986677efbe2b9
Luke King <lkingfrm@gmail.com>
2019-03-11 14:43:33 -0600

    added task schema, updated list schema

--- server/server-assets/models/list.js
+++ server/server-assets/models/list.js
@@ -3,16 +3,37 @@ let Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId
 let schemaName = 'List'
 
+let Task = require('./task')
 
 let schema = new Schema({
-  title: { type: String, required: true },
-  authorId: { type: ObjectId, ref: 'User', required: true },
-  boardId: { type: ObjectId, ref: 'Board', required: true }
-}, { timestamps: true })
+  title: {
+    type: String,
+    required: true
+  },
+  authorId: {
+    type: ObjectId,
+    ref: 'User',
+    required: true
+  },
+  boardId: {
+    type: ObjectId,
+    ref: 'Board',
+    required: true
+  }
+}, {
+  timestamps: true
+})
 
 //CASCADE ON DELETE
 
-
+schema.pre('remove', function (next) {
+  //this._id
+  Promise.all([
+    Task.deleteMany({
+      boardId: this._id
+    })
+  ]).then(() => next()).catch(err => next(err))
+})
 
 
 module.exports = mongoose.model(schemaName, schema)
---
+++ server/server-assets/models/task.js
@@ -0,0 +1,45 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+let schemaName = 'Task'
+
+let comment = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  authorId: {
+    type: ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+let task = new Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  listId: {
+    type: ObjectId,
+    ref: 'List',
+    required: true
+  },
+  authorId: {
+    type: ObjectId,
+    ref: 'User',
+    required: true
+  },
+  boardid: {
+    type: ObjectId,
+    ref: 'Board',
+    required: true
+  },
+  comments: [comment]
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model(schemaName, task)
