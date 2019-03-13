a325701af4a930df8ec84f6da8325ea840aea7ab
Luke King <lkingfrm@gmail.com>
2019-03-13 11:18:38 -0600

    task delete done

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -2,7 +2,7 @@
   <div class="tasks">
     <div class="d-flex justify-content-between" v-for="task in tasks" :key="task._id">
       <h6 class="border-dark rounded m-1 p-1">{{task.description}}</h6>
-      <button class="btn  " @click="deleteTask(task._id)"><small><i class="fas fa-trash-alt"></i></small></button>
+      <button class="btn  " @click="deleteTask(task._id, task.listId)"><small><i class="fas fa-trash-alt"></i></small></button>
     </div>
   </div>
 </template>
@@ -23,8 +23,12 @@
     },
     props: ['tasks'],
     methods: {
-      deleteTask(id) {
-        // write this function
+      deleteTask(id, listId) {
+        let data = {
+          taskId: id,
+          listId: listId
+        }
+        this.$store.dispatch('deleteTask', data)
       }
     }
   };
