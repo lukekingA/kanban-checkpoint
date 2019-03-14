205e4f31cbdf7a718e36f0519e6caeaa2971f803
Michael Graves <msgraves99@gmail.com>
2019-03-14 13:53:26 -0600

    style change

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -7,8 +7,10 @@
         <div class="d-flex justify-content-between border-bottom m-2">
           <div class="d-flex justify-content-start">
             <div class="">
-              <button class="btn ml-0 py-0 my-1 text-success" @click="visComForm"><i class="fas fa-clipboard-list"></i></button>
-              <button class="btn ml-0 py-0 my-1" @click="visEditForm"><small><i class="fas fa-pencil-alt"></i></small></button>
+              <button class="btn ml-0 py-0 my-1 text-success" @click="visComForm"><i
+                  class="fas fa-clipboard-list"></i></button>
+              <button class="btn ml-0 py-0 my-1" @click="visEditForm"><small><i
+                    class="fas fa-pencil-alt"></i></small></button>
             </div>
             <div class="text-left">
               <h6 class="border-dark rounded m-1 mb-0 p-1">{{task.description}}</h6>
@@ -16,14 +18,16 @@
           </div>
           <div class="">
             <input class="btn" type="checkbox" name="completed" :checked="task.completed" @click="taskCompleted(task)">
-            <button class="btn  " @click="deleteTask(task._id, task.listId)"><small><i class="fas fa-trash-alt"></i></small></button>
+            <button class="btn  " @click="deleteTask(task._id, task.listId)"><small><i
+                  class="fas fa-trash-alt"></i></small></button>
           </div>
         </div>
       </drag>
       <drop class="drop" @drop="handleDrop"></drop>
       <div v-show="showEditForm">
         <form @submit.prevent="editTask(task)">
-          <input class="rounded pl-1" ref="editForm" type="text" v-model="taskDescription" placeholder="task description">
+          <input class="rounded pl-1" ref="editForm" type="text" v-model="taskDescription"
+            placeholder="task description">
         </form>
 
       </div>
@@ -34,9 +38,11 @@
 
       </div>
       <div>
-        <div :class="comStyle" class="d-flex rounded shadow-sm justify-content-between ml-5 my-2 mr-2" v-for="comment in task.comments">
+        <div :class="task.completed ? 'tsk2-complete' : 'bg-white'"
+          class="d-flex rounded shadow-sm justify-content-between ml-5 my-2 mr-2" v-for="comment in task.comments">
           <p class="pl-2 py-1 text-left my-1">{{comment.content}}</p>
-          <button class="btn mr-1" @click="deleteComment(task._id, task.listId, comment._id)"><small><i class="fas fa-trash-alt"></i></small></button>
+          <button class="btn mr-1" @click="deleteComment(task._id, task.listId, comment._id)"><small><i
+                class="fas fa-trash-alt"></i></small></button>
         </div>
       </div>
     </div>
@@ -69,8 +75,9 @@
         return 'bg-light'
       },
       comStyle() {
+        debugger
         if (this.task.completed) {
-          return 'tsk-complete'
+          return 'tsk2-complete'
         }
         return 'bg-white'
       }
@@ -152,6 +159,10 @@
   }
 
   .tsk-complete {
-    background-color: rgb(230, 218, 238);
+    background-color: rgb(220, 218, 238);
+  }
+
+  .tsk2-complete {
+    background-color: rgb(187, 180, 216);
   }
 </style>
